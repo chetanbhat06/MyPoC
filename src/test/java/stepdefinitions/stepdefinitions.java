e6d369cb31369b787b167478a761f84ebbd2f63e
chetanbhat06 <cjbhat@yahoo.com>
2017-12-25 14:37:34 +0530

    Cucumber tags filters

--- src/test/java/stepdefinitions/stepdefinitions.java
+++ src/test/java/stepdefinitions/stepdefinitions.java
@@ -7,30 +7,30 @@ public class stepdefinitions {
     @When("^I have a$")
     public void i_have_a() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("#1");
+        System.out.println("First #1");
     }
 
     @When("^I have b$")
     public void i_have_b() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("#2");
+        System.out.println("Second #2");
     }
 
     @Then("^display sum$")
     public void display_sum() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("#3");
+        System.out.println("Sum #3");
     }
 
     @Then("^display multiplication$")
     public void display_multiplication() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("#4");
+        System.out.println("Multiplication #4");
     }
     @Then("^display division$")
     public void display_division() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("#5");
+        System.out.println("Division #5");
     }
 
 }
