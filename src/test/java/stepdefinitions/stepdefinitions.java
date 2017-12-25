9b267a21ada5b37cb2e29259606a31edd02052f9
chetanbhat06 <cjbhat@yahoo.com>
2017-12-25 19:07:28 +0530

    Cucumber tags filters

--- src/test/java/stepdefinitions/stepdefinitions.java
+++ src/test/java/stepdefinitions/stepdefinitions.java
@@ -3,6 +3,9 @@ package stepdefinitions;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class stepdefinitions {
     int a, b, c;
     @When("^I have a$")
@@ -11,6 +14,10 @@ public class stepdefinitions {
         System.out.println("First #1");
         a = 5;
         System.out.println("A : "+a);
+        FileWriter writer = new FileWriter("MyFile.txt", true);
+        writer.write("I have a");
+        writer.write("\r\n");   // write new line
+
     }
 
     @When("^I have b$")
@@ -19,6 +26,9 @@ public class stepdefinitions {
         System.out.println("Second #2");
         b = 10;
         System.out.println("A : "+b);
+        FileWriter writer = new FileWriter("MyFile.txt", true);
+        writer.write("I have b");
+        writer.write("\r\n");   // write new line
     }
 
     @Then("^display sum$")
@@ -28,6 +38,10 @@ public class stepdefinitions {
         c = a+b;
         System.out.println("Sum : "+c);
 
+        FileWriter writer = new FileWriter("MyFile.txt", true);
+        writer.write("I have Sum");
+        writer.write("\r\n");   // write new line
+
     }
 
     @Then("^display multiplication$")
@@ -36,6 +50,10 @@ public class stepdefinitions {
         System.out.println("Multiplication #4");
         c = a*b;
         System.out.println("Multiplication : "+c);
+
+        FileWriter writer = new FileWriter("MyFile.txt", true);
+        writer.write("I have Multiplication");
+        writer.write("\r\n");   // write new line
     }
     @Then("^display division$")
     public void display_division() throws Throwable {
@@ -43,6 +61,10 @@ public class stepdefinitions {
         System.out.println("Division #5");
         c = b/a;
         System.out.println("Division : "+c);
+
+        FileWriter writer = new FileWriter("MyFile.txt", true);
+        writer.write("I have Division");
+        writer.write("\r\n");   // write new line
     }
 
 }
